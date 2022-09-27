eae79625f8b64fbf1cee01afc1de4ad6637243ae
Vicki Wong <vicki.wong85@gmail.com>
2022-09-26 21:30:04 -0500

    added all categories to find quiz

--- src/components/FindQuiz.jsx
+++ src/components/FindQuiz.jsx
@@ -6,6 +6,7 @@ const FindQuiz = (props) => {
       <h1>Find Quiz</h1>
         <label>Category: </label>
         <select onChange={props.changeCategory}>
+          <option value="">Random</option>
           <option value="9" >General Knowledge</option>
           <option value="10">Entertainment: Books</option>
           <option value="11">Entertainment: Film</option>
@@ -14,6 +15,22 @@ const FindQuiz = (props) => {
           <option value="14">Entertainment: Television</option>
           <option value="15">Entertainment: Video Games</option>
           <option value="16">Entertainment: Board Games</option>
+          <option value="17">Science and Nature</option>
+          <option value="18">Science: Computers</option>
+          <option value="19">Science: Mathematics</option>
+          <option value="20">Mythology</option>
+          <option value="21">Sports</option>
+          <option value="22">Geography</option>
+          <option value="23">History</option>
+          <option value="24">Politics</option>
+          <option value="25">Art</option>
+          <option value="26">Celebrities</option>
+          <option value="27">Animals</option>
+          <option value="28">Vehicles</option>
+          <option value="29">Entertainment: Comics</option>
+          <option value="30">Science: Gadgets</option>
+          <option value="31">Entertainment: Japanese Anime and Manga</option>
+          <option value="32">Entertainment: Cartoon and Animations</option>
         </select>
         <br></br>
         <label>Difficulty: </label>
