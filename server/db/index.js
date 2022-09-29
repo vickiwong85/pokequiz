6c820a98abc64c3b39f6e602023be4783cf6e65a
Vicki Wong <vicki.wong85@gmail.com>
2022-09-29 12:13:44 -0500

    edit auth0

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const { Pool, Client } = require('pg')
 const path = require('path');
 // require('dotenv').config({ path: '../.env' });
-require('dotenv').config({path: path.resolve(__dirname, '.env')});
+require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 
 // const pool = new Pool({
 //   host: "localhost",
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Auth0Provider } from "@auth0/auth0-react";
+// import { Auth0Provider } from "@auth0/auth0-react";
 import App from './app.jsx';
 
 //new syntax for React 18
@@ -8,12 +8,6 @@ const container = document.getElementById('root');
 document.body.appendChild(container);
 const root = createRoot(container);
 root.render(
-  <Auth0Provider
-    domain="dev-44x2pjwg.us.auth0.com"
-    clientId="CMNvCZs2XMudP25x7vkCCOWwuOKzT8Ei"
-    redirectUri={window.location.origin}
-  >
   <App/>
-  </Auth0Provider>
 
 );
