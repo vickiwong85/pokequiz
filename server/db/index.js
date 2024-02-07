7388aa0a5ae8729ac47dbd7b2451d14a8915c4e6
Vicki Wong <vicki.wong85@gmail.com>
2024-02-06 19:14:26 -0800

    clean up console logs

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,6 @@ const { Pool, Client } = require('pg')
 const path = require('path');
 require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 
-console.log('hello');
-console.log(process.env.HOST);
 const pool = new Pool({
   host: process.env.HOST,
   user: process.env.USERNAME,
